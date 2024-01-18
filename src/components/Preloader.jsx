22ddd0da82d6d400a687628ee2eb03ec9f06292a
shreya1569AS <bshreya932@gmail.com>
2024-01-18 16:50:46 +0530

    loader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -5,12 +5,13 @@ const Preloader = () => {
     <div>
       <div className=" h-screen fixed top-0 left-0 right-0 overflow-hidden flex justify-center items-center  space-x-2  bg-[#243040] flex-col">
         <img src={logo} alt="img" className="animate-bounce" />
-         <div className="flex gap-2">
+         {/* <div className="flex gap-2">
           <span className="sr-only">Loading...</span>
           <div className="h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
           <div className="h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></div>
           <div className="h-8 w-8 bg-white rounded-full animate-bounce"></div>
-        </div>
+        </div> */}
+        <h1 className="text-4xl text-white font-bold font-roboto animate-pulse">FURNITURE</h1>
       </div>
     </div>
   );
